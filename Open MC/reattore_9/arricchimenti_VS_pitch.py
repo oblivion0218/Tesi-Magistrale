1a594c343b4643ea7396a4c8728d628eecc3efe6
Riccardo Bossi <bossi.ricky@gmail.com>
2026-04-13 18:22:08 +0200

    .

--- Open MC/reattore_9/arricchimenti_VS_pitch.py
+++ Open MC/reattore_9/arricchimenti_VS_pitch.py
@@ -58,7 +58,7 @@ warnings.filterwarnings("ignore", category=UserWarning, module="papermill")
 # FASE 1: RICERCA ARRICCHIMENTO (SURROGATE MODELING - PCHIP)
 # ==========================================
 
-list_i = [1.25]  
+list_i = [1.875]  
 a_m = 0.05     
 w = 0.15          
 
@@ -91,7 +91,7 @@ for m in list_i:
     p = df_kmax['Pressione_atm'].iloc[iter_kmax]
     
     # Punti di partenza per inizializzare il modello
-    a_vals = [0.05, 0.02]
+    a_vals = [0.03, 0.02]
     k_vals_dict = {}
     converged = False
     best_delta_k = np.inf
